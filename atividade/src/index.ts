c131d28b0796884518135f1ea351a9574d46e246
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-07-24 17:31:35 -0300

    atividade03

--- atividade/src/index.ts
+++ atividade/src/index.ts
@@ -1,21 +1,42 @@
-import fastify from "fastify";
+import fastify, { FastifyRequest } from "fastify";
+import { IUser } from "./interfaces/users.interface";
 
 const app = fastify();
 
-app.post("/users", (req, res) => {
-    return res.send({ message: "Eu cadastro um usuário."});
-})
+const usersDatabase: IUser[] = [];
 
-app.patch("/users/password", (req, res) => {
-    return res.send({ message: "Eu atualizo a senha do usuário."});
-})
+let id = 1;
 
-app.get("/users", (req, res) => {
-    return res.send({ message: "Eu trago o usuário."});
+app.post("/users", (req: FastifyRequest<{ Body: Omit<IUser, "id"> }>, res) => {
+  const { name, email, password } = req.body;
+
+  const newUser: IUser = {
+    id,
+    name,
+    email,
+    password,
+  };
+
+  usersDatabase.push(newUser);
+
+  id++;
+
+  return res.send({ id: newUser.id, name: newUser.name, email: newUser.email });
 });
+
+app.get(
+  "/users/:id",
+  (req: FastifyRequest<{ Params: { id: string } }>, res) => {
+    const user = usersDatabase.find(
+      (user) => user.id === Number(req.params.id)
+    ) as IUser;
+
+    return res.send({ id: user.id, name: user.name, email: user.email });
+  }
+);
 
 const PORT = 3001;
 
 app.listen({ port: PORT }, () => {
-    console.log(`API sucessfully started at port ${PORT}`);
+  console.log(`API sucessfully started at port ${PORT}`);
 });
