7b3efc6359183b971c63259bbb8c751f56308066
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-07-24 13:02:16 -0300

    fastify instalation and hello world

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Olá mundo.")
+import fastify from "fastify";
+
+const app = fastify();
+
+// Verbos HTTP
+// Get - rotas de leitura
+// Post - rotas de cadastro / inserção de dados
+// Delete - rotas de exclusão
+// Patch / Put - rotas de atualização de dados
+
+app.get("/hello", (req, res) => {
+    return res.send({ message: "Olá, estou aprendendo back-end!"});
+});
+
+const PORT = 3001;
+
+app.listen({ port: PORT }, () => {
+    console.log("API successfully started at port " + PORT)
+});
