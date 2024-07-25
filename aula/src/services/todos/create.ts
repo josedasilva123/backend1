c05a384a36eb6d1597c74ce89922400f3d4b4d4d
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-07-25 18:24:59 -0300

    create todo error

--- aula/src/services/todos/create.ts
+++ aula/src/services/todos/create.ts
@@ -1,7 +1,12 @@
 import { generateId, todoDatabase } from "../../database/todos.database";
+import { AppError } from "../../errors/AppError";
 import { ITodo } from "../../interfaces/todos.interface";
 
 export function create(body: Omit<ITodo, "id">) {
+  if(!body.title || !body.content){
+    throw new AppError("Missing body parameter (should be title and content)", 400);
+  }
+
   const newTodo: ITodo = {
     id: generateId(),
     title: body.title,
