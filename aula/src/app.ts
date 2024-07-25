94590a0ca54a50ebcd6430404f6d9e9e9d50ef7c
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-07-25 18:12:06 -0300

    custom error class

--- aula/src/app.ts
+++ aula/src/app.ts
@@ -1,12 +1,10 @@
 import fastify from "fastify";
 import { todosRoutes } from "./routes/todos.routes";
+import { errorHandler } from "./errors/errorHandler";
 
 export const app = fastify({
   logger: true,
 });
 
-app.setErrorHandler((error, req, res) => {
-  return res.status(400).send({ error: error.message });
-});
-
+app.setErrorHandler(errorHandler);
 app.register(todosRoutes, { prefix: "/todos"});
--- aula/src/services/todos/remove.ts
+++ aula/src/services/todos/remove.ts
@@ -1,10 +1,11 @@
 import { todoDatabase } from "../../database/todos.database";
+import { AppError } from "../../errors/AppError";
 
 export function remove(id: number){
     const index = todoDatabase.findIndex(todo => todo.id === Number(id));
     
     if(index === -1){
-        throw new Error("Todo not found.");
+        throw new AppError("Todo not found.", 404);
     }
 
     todoDatabase.splice(index, 1);
