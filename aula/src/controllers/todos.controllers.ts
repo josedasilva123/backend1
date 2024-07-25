9e967a4cef7b2c82b50eab14faf3341bf55b3094
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-07-25 17:30:05 -0300

    status code

--- aula/src/controllers/todos.controllers.ts
+++ aula/src/controllers/todos.controllers.ts
@@ -5,19 +5,19 @@ import { todosService } from "../services/todos/_index";
 function create(req: FastifyRequest<{ Body: Omit<ITodo, "id"> }>, res: FastifyReply){
     const todo = todosService.create(req.body);
 
-    return res.send(todo);
+    return res.status(201).send(todo);
 }
 
 function getMany(req: FastifyRequest, res: FastifyReply){
     const todoList = todosService.getMany();
 
-    return res.send(todoList);
+    return res.status(200).send(todoList);
 }
 
 function remove(req: FastifyRequest<{ Params: { id: string }}>, res: FastifyReply){
     const message = todosService.remove(Number(req.params.id));
 
-    return res.send(message);
+    return res.status(200).send(message);
 }
 
 export const todosControllers = { create, getMany, remove };
