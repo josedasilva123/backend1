f7c72f5006e98e700f797a7007c51e96a83cbc82
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-07-26 15:05:20 -0300

    interface and database

---
+++ projeto/src/database/products.database.ts
@@ -0,0 +1,10 @@
+import { IProduct } from "../interfaces/products.interface";
+
+export const productsDatabase: IProduct[] = [];
+
+let id = 0;
+
+export function generateId(){
+    id++;
+    return;
+}
