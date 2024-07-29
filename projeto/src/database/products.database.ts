dd544b0a354c05849df465d813b449c48cb9e075
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-07-29 19:56:20 -0300

    product in database

--- projeto/src/database/products.database.ts
+++ projeto/src/database/products.database.ts
@@ -1,8 +1,16 @@
 import { IProduct } from "../interfaces/products.interface";
 
-export const productsDatabase: IProduct[] = [];
+export const productsDatabase: IProduct[] = [
+    {
+        id: 1,
+        name: "Computador",
+        description: "Computador bonito",
+        price: 3000,
+        createdAt: new Date(),
+    }
+];
 
-let id = 0;
+let id = 1;
 
 export function generateId(){
     id++;
