abd2f2ef1f57764bb374c3cccc160cc5a51059ea
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-08-05 12:54:03 -0300

    cors

--- projeto/src/app.ts
+++ projeto/src/app.ts
@@ -1,8 +1,10 @@
 import fastify from "fastify";
+import fastifyCors from "@fastify/cors";
 import { productsRouter } from "./routes/products.routes";
 import { errorHandler } from "./errors/errorHandler";
 
 export const app = fastify();
 
+app.register(fastifyCors);
 app.setErrorHandler(errorHandler);
 app.register(productsRouter, { prefix: "/products"});
